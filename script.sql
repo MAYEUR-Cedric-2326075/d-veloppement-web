d196c0b93a6a7c504113f817e0bd47699a61da1f
SIMON Louis <146358792+SIMON-Louis-2326101aa@users.noreply.github.com>
2024-09-18 09:08:00 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -71,6 +71,7 @@ create table repas(
     id_repas int primary key,
     id_grade serial,
     dat date,
-    FOREIGN KEY (id_grade) REFERENCES grade(id_grade)
+    FOREIGN KEY (id_grade) REFERENCES grade(id_grade),
+    FOREIGN KEY (dat) REFERENCES dat(dat)
 );
 

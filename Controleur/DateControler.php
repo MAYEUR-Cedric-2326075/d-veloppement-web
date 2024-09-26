9194a1dee3d8f6aef4a990523c6f848dc3d76768
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-09-26 17:27:38 +0200

    Update DateControler.php

--- Controleur/DateControler.php
+++ Controleur/DateControler.php
@@ -1,11 +1,87 @@
 <?php
 
 require_once 'Noyau/ConnexionBD.php';
+require_once 'Modele/ModeleDate.php';
 
-class DateControler {
+class DateController {
 
-    // Classe vide pour l'instant
+    private $dateModel;
 
+
+    public function __construct($db) {
+
+        $this->dateModel = new DateModel($db);
+    }
+
+
+    public function createDate($data) {
+
+        if (!isset($data['dat'])) {
+            return "Champ date obligatoire.";
+        }
+
+        $data['dat'] = date('Y-m-d', strtotime($data['dat']));
+
+        $this->dateModel->createDate($data);
+        return "Date créée.";
+    }
+
+
+    public function getDateById($id) {
+
+        if (!is_numeric($id)) {
+            return "ID invalide.";
+        }
+
+        $date = $this->dateModel->getDateById($id);
+        if ($date) {
+            return $date;
+        } else {
+            return "Date non trouvée.";
+        }
+    }
+
+
+    public function getAllDates() {
+
+        return $this->dateModel->getAllDates();
+    }
+
+
+    public function updateDate($id, $data) {
+
+        if (!is_numeric($id)) {
+            return "ID invalide.";
+        }
+
+        if (!isset($data['dat'])) {
+            return "Champ date obligatoire.";
+        }
+
+        $data['dat'] = date('Y-m-d', strtotime($data['dat']));
+
+        $updated = $this->dateModel->updateDate($id, $data);
+        if ($updated) {
+            return "Date mise à jour.";
+        } else {
+            return "Erreur de la mise à jour de la date.";
+        }
+    }
+
+
+    public function deleteDate($id) {
+
+        if (!is_numeric($id)) {
+            return "ID invalide.";
+        }
+
+        $deleted = $this->dateModel->deleteDate($id);
+        if ($deleted) {
+            return "Date supprimée.";
+        } else {
+            return "Erreur suppression de la date.";
+        }
+    }
 }
 
 ?>

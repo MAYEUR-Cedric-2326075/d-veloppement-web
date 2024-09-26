1561d7b1b23c5d78e703fa5810a19681d437dabc
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-09-26 14:34:44 +0200

    Update ModeleDignity.php

--- Modele/ModeleDignity.php
+++ Modele/ModeleDignity.php
@@ -1,7 +1,12 @@
 <?php
 
 class ModeleDignity {
-    // Votre code ici
+    public function getDignity() {
+        $sql = "SELECT * FROM id_dignity";
+        $stmt = $this->pdo->getPdo()->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
 }
 
 ?>

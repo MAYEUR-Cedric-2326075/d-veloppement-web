1cc0900191dce356d11721cc4017becea908cef7
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-09-29 23:42:40 +0200

    Update MealControler.php

--- Controleur/MealControler.php
+++ Controleur/MealControler.php
@@ -2,10 +2,56 @@
 
 require_once 'Noyau/ConnexionBD.php';
 
-class MealControler {
+class MealController {
 
-    // Classe vide pour l'instant
+    private $pdo;
 
+    public function __construct() {
+        $this->pdo = ConnectionBD::getInstance()->getPdo();
+    }
+
+    public function createMeal($data) {
+        $sql = "INSERT INTO Meal (dat, address, id_organizer, id_club) VALUES (:dat, :address, :id_organizer, :id_club)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':dat', $data['dat']);
+        $stmt->bindParam(':address', $data['address']);
+        $stmt->bindParam(':id_organizer', $data['id_organizer']);
+        $stmt->bindParam(':id_club', $data['id_club']);
+        return $stmt->execute();
+    }
+
+    public function getMeal($id) {
+        $sql = "SELECT * FROM Meal WHERE id_meal = :id_meal";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_meal', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllMeals() {
+        $sql = "SELECT * FROM Meal";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function updateMeal($id, $data) {
+        $sql = "UPDATE Meal SET dat = :dat, address = :address, id_organizer = :id_organizer, id_club = :id_club WHERE id_meal = :id_meal";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_meal', $id);
+        $stmt->bindParam(':dat', $data['dat']);
+        $stmt->bindParam(':address', $data['address']);
+        $stmt->bindParam(':id_organizer', $data['id_organizer']);
+        $stmt->bindParam(':id_club', $data['id_club']);
+        return $stmt->execute();
+    }
+
+    public function deleteMeal($id) {
+        $sql = "DELETE FROM Meal WHERE id_meal = :id_meal";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_meal', $id);
+        return $stmt->execute();
+    }
 }
 
 ?>

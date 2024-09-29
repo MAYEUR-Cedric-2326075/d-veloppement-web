dfe5aa011b2cce7d5a462db2067ce2b2c1f8d58b
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-09-29 23:44:03 +0200

    Update SexControler.php

--- Controleur/SexControler.php
+++ Controleur/SexControler.php
@@ -2,10 +2,50 @@
 
 require_once 'Noyau/ConnexionBD.php';
 
-class SexControler {
+class SexController {
 
-    // Classe vide pour l'instant
+    private $pdo;
 
+    public function __construct() {
+        $this->pdo = ConnectionBD::getInstance()->getPdo();
+    }
+
+    public function createSex($data) {
+        $sql = "INSERT INTO Sex (libelle_sex) VALUES (:libelle)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':libelle', $data['libelle']);
+        return $stmt->execute();
+    }
+
+    public function getSex($id) {
+        $sql = "SELECT * FROM Sex WHERE id_sex = :id_sex";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_sex', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllSexes() {
+        $sql = "SELECT * FROM Sex";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function updateSex($id, $data) {
+        $sql = "UPDATE Sex SET libelle_sex = :libelle WHERE id_sex = :id_sex";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_sex', $id);
+        $stmt->bindParam(':libelle', $data['libelle']);
+        return $stmt->execute();
+    }
+
+    public function deleteSex($id) {
+        $sql = "DELETE FROM Sex WHERE id_sex = :id_sex";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_sex', $id);
+        return $stmt->execute();
+    }
 }
 
 ?>

02c8cb3ac6d6fa660234cdc72be052c3751a1675
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-09-29 23:42:55 +0200

    Update PlatControler.php

--- Controleur/PlatControler.php
+++ Controleur/PlatControler.php
@@ -2,10 +2,52 @@
 
 require_once 'Noyau/ConnexionBD.php';
 
-class PlatControler {
+class PlatController {
 
-    // Classe vide pour l'instant
+    private $pdo;
 
+    public function __construct() {
+        $this->pdo = ConnectionBD::getInstance()->getPdo();
+    }
+
+    public function createPlat($data) {
+        $sql = "INSERT INTO Plat (libelle_plat, id_dish) VALUES (:libelle, :id_dish)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':libelle', $data['libelle']);
+        $stmt->bindParam(':id_dish', $data['id_dish']);
+        return $stmt->execute();
+    }
+
+    public function getPlat($id) {
+        $sql = "SELECT * FROM Plat WHERE id_plat = :id_plat";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_plat', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllPlats() {
+        $sql = "SELECT * FROM Plat";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function updatePlat($id, $data) {
+        $sql = "UPDATE Plat SET libelle_plat = :libelle, id_dish = :id_dish WHERE id_plat = :id_plat";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_plat', $id);
+        $stmt->bindParam(':libelle', $data['libelle']);
+        $stmt->bindParam(':id_dish', $data['id_dish']);
+        return $stmt->execute();
+    }
+
+    public function deletePlat($id) {
+        $sql = "DELETE FROM Plat WHERE id_plat = :id_plat";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_plat', $id);
+        return $stmt->execute();
+    }
 }
 
 ?>

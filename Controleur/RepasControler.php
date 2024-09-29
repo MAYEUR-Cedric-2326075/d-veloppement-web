3aab27f3c2389a211d36a3b4f4c1eaf40d508222
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-09-29 23:43:39 +0200

    Update RepasControler.php

--- Controleur/RepasControler.php
+++ Controleur/RepasControler.php
@@ -2,10 +2,52 @@
 
 require_once 'Noyau/ConnexionBD.php';
 
-class RepasControler {
+class RepasController {
 
-    // Classe vide pour l'instant
+    private $pdo;
 
+    public function __construct() {
+        $this->pdo = ConnectionBD::getInstance()->getPdo();
+    }
+
+    public function createRepas($data) {
+        $sql = "INSERT INTO Repas (libelle_repas, id_meal) VALUES (:libelle, :id_meal)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':libelle', $data['libelle']);
+        $stmt->bindParam(':id_meal', $data['id_meal']);
+        return $stmt->execute();
+    }
+
+    public function getRepas($id) {
+        $sql = "SELECT * FROM Repas WHERE id_repas = :id_repas";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_repas', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllRepas() {
+        $sql = "SELECT * FROM Repas";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function updateRepas($id, $data) {
+        $sql = "UPDATE Repas SET libelle_repas = :libelle, id_meal = :id_meal WHERE id_repas = :id_repas";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_repas', $id);
+        $stmt->bindParam(':libelle', $data['libelle']);
+        $stmt->bindParam(':id_meal', $data['id_meal']);
+        return $stmt->execute();
+    }
+
+    public function deleteRepas($id) {
+        $sql = "DELETE FROM Repas WHERE id_repas = :id_repas";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_repas', $id);
+        return $stmt->execute();
+    }
 }
 
 ?>

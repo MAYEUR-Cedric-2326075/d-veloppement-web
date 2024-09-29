00ff620a438c43f325163d75e9a7c947868cc0e1
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-09-29 23:44:44 +0200

    Update TitleControler.php

--- Controleur/TitleControler.php
+++ Controleur/TitleControler.php
@@ -2,10 +2,50 @@
 
 require_once 'Noyau/ConnexionBD.php';
 
-class TitleControler {
+class TitleController {
 
-    // Classe vide pour l'instant
+    private $pdo;
 
+    public function __construct() {
+        $this->pdo = ConnectionBD::getInstance()->getPdo();
+    }
+
+    public function createTitle($data) {
+        $sql = "INSERT INTO Title (libelle_title) VALUES (:libelle)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':libelle', $data['libelle']);
+        return $stmt->execute();
+    }
+
+    public function getTitle($id) {
+        $sql = "SELECT * FROM Title WHERE id_title = :id_title";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_title', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllTitles() {
+        $sql = "SELECT * FROM Title";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function updateTitle($id, $data) {
+        $sql = "UPDATE Title SET libelle_title = :libelle WHERE id_title = :id_title";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_title', $id);
+        $stmt->bindParam(':libelle', $data['libelle']);
+        return $stmt->execute();
+    }
+
+    public function deleteTitle($id) {
+        $sql = "DELETE FROM Title WHERE id_title = :id_title";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id_title', $id);
+        return $stmt->execute();
+    }
 }
 
 ?>

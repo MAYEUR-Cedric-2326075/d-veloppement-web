f154d7675da350d7f5dfe4c696f8032f053c49b1
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-10-01 21:52:04 +0200

    Update ModeleMeal.php

--- Modele/ModeleMeal.php
+++ Modele/ModeleMeal.php
@@ -1,7 +1,44 @@
 <?php
 
+require_once './../Noyau/ConnexionBD.php';
+
 class ModeleMeal {
-    // Votre code ici
+    private $pdo;
+
+    public function __construct() {
+        $this->pdo = ConnectionBD::getInstance();
+    }
+
+    public function createMeal(string $mealName, string $description): void {
+        $parameter = ['mealName' => $mealName, 'description' => $description];
+        $resultToInsert = $this->pdo->insert('Meal', $parameter);
+    }
+
+    public function deleteMeal(string $id_meal): bool {
+        $where = "id_meal = '$id_meal'";
+        return $this->pdo->delete('Meal', $where);
+    }
+
+    public function updateMeal(string $id_meal, array $data): void {
+        $where = "id_meal = '$id_meal'";
+        $this->pdo->update('Meal', $data, $where);
+    }
+
+    public function getAllMeals(): array {
+        return $this->pdo->getAll("Meal");
+    }
+
+    public function getMealById(string $id_meal): array {
+        return $this->pdo->getAll("Meal", "id_meal", $id_meal);
+    }
+
+    public function getMealName(string $id_meal): string {
+        return $this->pdo->getElement("Meal", "mealName", "id_meal", $id_meal);
+    }
+
+    public function getMealDescription(string $id_meal): string {
+        return $this->pdo->getElement("Meal", "description", "id_meal", $id_meal);
+    }
 }
 
 ?>
